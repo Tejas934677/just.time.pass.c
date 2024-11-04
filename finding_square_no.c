87f0d8bb8d5a9d9712825ea5ee89f1ae17780231
Tejas gowda.S <tgowda934@gmail.com>
2024-11-04 20:16:43 +0530

    click here

---
+++ finding_square_no.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+    int t  = 0 ;
+    while(t<2){
+
+    
+    int n;
+    double m;
+    printf("\nEnter the real number that you wanted to convert to the converted by root number\n >>");
+    scanf("%d",&n);
+    m = sqrt(n);
+    printf("Your number is %f\n",m );
+
+}}
