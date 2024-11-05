63c2f60561d0da92fcb100512582c4eedba182c3
Tejas gowda.S <tgowda934@gmail.com>
2024-11-05 22:32:25 +0530

    sqrt.c

---
+++ sqrt_no.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    int sqrT;
+    float g;
+    printf("Enter the square number that you wanted to convert real number \n√");
+    scanf("%d",&sqrT);
+    g = sqrt(sqrT);
+    printf("you got the real number %f", g);
+
+}
